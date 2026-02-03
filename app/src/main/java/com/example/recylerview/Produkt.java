e3ffbff27061b28935ce061af8cb46a30a39e093
skpl57 <kania.skpl57@gmail.com>
2026-02-03 07:44:01 +0100

    widok na przedmiot

--- app/src/main/java/com/example/recylerview/Produkt.java
+++ app/src/main/java/com/example/recylerview/Produkt.java
@@ -3,5 +3,44 @@ package com.example.recylerview;
 public class Produkt {
     private String nazwaProduktu;
     private boolean czyKupione;
+    private int cena;
 
+    public Produkt(String nazwaProduktu, int cena) {
+        this.nazwaProduktu = nazwaProduktu;
+        this.cena = cena;
+        czyKupione = false;
+    }
+
+    public String getNazwaProduktu() {
+        return nazwaProduktu;
+    }
+
+    public void setNazwaProduktu(String nazwaProduktu) {
+        this.nazwaProduktu = nazwaProduktu;
+    }
+
+    public boolean isCzyKupione() {
+        return czyKupione;
+    }
+
+    public void setCzyKupione(boolean czyKupione) {
+        this.czyKupione = czyKupione;
+    }
+
+    public int getCena() {
+        return cena;
+    }
+
+    public void setCena(int cena) {
+        this.cena = cena;
+    }
+
+    @Override
+    public String toString() {
+        return "Produkt{" +
+                "nazwaProduktu='" + nazwaProduktu + '\'' +
+                ", czyKupione=" + czyKupione +
+                ", cena=" + cena +
+                '}';
+    }
 }
