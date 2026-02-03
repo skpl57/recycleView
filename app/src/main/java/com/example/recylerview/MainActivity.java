66f88dd95ecbb9aec9989df0ec026e47590b3454
skpl57 <kania.skpl57@gmail.com>
2026-02-03 08:45:22 +0100

    wyswietlanie listy

--- app/src/main/java/com/example/recylerview/MainActivity.java
+++ app/src/main/java/com/example/recylerview/MainActivity.java
@@ -7,8 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Produkt> produkty;
+    private RecyclerView recyclerView;
+    private ZakupyAdapter zakupyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        produkty.add(new Produkt("masło", 5));
+        produkty.add(new Produkt("czekolada", 3));
+        produkty.add(new Produkt("mleko", 2));
+        produkty.add(new Produkt("jogurt", 7));
+        produkty.add(new Produkt("w pysk", 1));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        zakupyAdapter = new ZakupyAdapter(MainActivity.this, produkty);
+
+        recyclerView.setAdapter(zakupyAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
