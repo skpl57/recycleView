ce41f7e572266239b90c5740ff15f2a863d95557
skpl57 <kania.skpl57@gmail.com>
2026-02-03 08:37:11 +0100

    cos nawet nie wiem co

--- app/src/main/java/com/example/recylerview/ZakupyAdapter.java
+++ app/src/main/java/com/example/recylerview/ZakupyAdapter.java
@@ -1,32 +1,52 @@
 package com.example.recylerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class ZakupyAdapter extends RecyclerView.Adapter<ZakupyAdapter.ProduktViewHolder>{
+
+    private ArrayList<Produkt> produkty;
+    private LayoutInflater inflater;
+
+
+    public ZakupyAdapter(Context context, ArrayList<Produkt> produkty) {
+        this.produkty = produkty;
+        inflater = LayoutInflater.from(context);
+    }
 
     @NonNull
     @Override
     public ProduktViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = inflater.inflate(R.layout.produkt_layout, parent, false);
+        return new ProduktViewHolder(itemView, this);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProduktViewHolder holder, int position) {
-
+        holder.checkBoxView.setText(produkty.get(position).toString());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return produkty.size();
     }
 
     public class ProduktViewHolder extends RecyclerView.ViewHolder{
-        public ProduktViewHolder(@NonNull View itemView) {
+        public CheckBox checkBoxView;
+        public ZakupyAdapter zakupyAdapter; // final????
+
+        public ProduktViewHolder(@NonNull View itemView, ZakupyAdapter adapter) {
             super(itemView);
+            checkBoxView = itemView.findViewById(R.id.checkBox);
+            zakupyAdapter = adapter;
         }
     }
 }
